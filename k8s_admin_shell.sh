4c2fe7f801bc6bce2894a44d14eb39566917b215
fritz duchardt <fritz@duchardt.net>
2021-07-26 06:48:54 +0200

    Separated privileged from unprivileged K8s manifest

--- k8s_admin_shell.sh
+++ k8s_admin_shell.sh
@@ -11,14 +11,14 @@
 set -euo pipefail
 
 privileged=${1:-false}
-k8s_yaml="k8s-admin-shell.yaml"
+k8s_yaml="k8s/k8s-admin-shell.yaml"
 command="bash"
 if $privileged; then
   echo "WARNING: Creating privileged host shell"
-  k8s_yaml="k8s-admin-privileged-shell.yaml"
+  k8s_yaml="k8s-privileged/k8s-admin-privileged-shell.yaml"
   command="nsenter --target 1 --mount --uts --ipc --net /bin/bash"
 fi
-kubectl apply -f "$(dirname "$0")/k8s/$k8s_yaml"
+kubectl apply -f "$(dirname "$0")/$k8s_yaml"
 # wait for pod to run
 while [ "$(kubectl get po k8s-admin-shell -o jsonpath='{.status.phase}')" != "Running" ]; do
   sleep 1
